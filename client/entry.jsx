5157e35ad640c65f349dd16072c19b547f3c69b9
yan <ian24257587@gmail.com>
2019-03-13 12:00:04 +0800

    project environment initialize~6~static(style unfinished), entry.jsx -> store.js

--- client/entry.jsx
+++ client/entry.jsx
@@ -1,2 +1,21 @@
 // @flow
 /* eslint global-require: 0 */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {
+  store,
+  history,
+} from './store.js';
+import App from './App.jsx';
+
+(async () => {
+  const root = document.getElementById('root');
+
+  if (root) {
+    ReactDOM.render(
+      <App store={store} history={history} />,
+      root,
+    );
+  }
+})();
