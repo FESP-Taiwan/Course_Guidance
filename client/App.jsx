2f743851c45288c04a3057e98c0274db1477fb46
yan <ian24257587@gmail.com>
2019-03-15 15:40:43 +0800

    project environment initialize~end

---
+++ client/App.jsx
@@ -0,0 +1,47 @@
+// @flow
+
+import React from 'react';
+import { MemoryRouter, Router } from 'react-router';
+import {
+  Switch,
+  Route,
+} from 'react-router-dom';
+import debug from 'debug';
+import { hot } from 'react-hot-loader';
+import { StyleRoot } from 'radium';
+import { Provider } from 'react-redux';
+
+// Containers
+
+// Debug mode
+if (process.env.NODE_ENV !== 'production') {
+  debug.enable('EZWeb:*');
+}
+
+const styles = {
+  root: {
+    width: '100%',
+    height: '100%',
+    display: 'block',
+    position: 'relative',
+  },
+};
+
+function App({
+  store,
+  history,
+}: any) {
+  return (
+    <Provider store={store}>
+      <MemoryRouter>
+        <StyleRoot style={styles.root}>
+          <Router history={history}>
+            <h1>Hello World</h1>
+          </Router>
+        </StyleRoot>
+      </MemoryRouter>
+    </Provider>
+  );
+}
+
+export default hot(module)(App);
---
+++ client/store.js
@@ -0,0 +1,32 @@
+// @flow
+/* eslint global-require: 0 */
+
+import thunk from 'redux-thunk';
+import {
+  createStore,
+  compose,
+  applyMiddleware,
+} from 'redux';
+import createHistory from 'history/createBrowserHistory';
+import { routerMiddleware } from 'react-router-redux';
+import fetchMiddleware from 'redux-middleware-fetch';
+import reducers from './reducers/index';
+
+/* eslint-enable */
+export const history = createHistory();
+
+const storeComposeArray = [applyMiddleware(
+  thunk,
+  routerMiddleware(history),
+  fetchMiddleware,
+)];
+
+export const store = createStore(
+  reducers,
+  {},
+  compose(...storeComposeArray),
+);
+
+if (module.hot) {
+  module.hot.accept('./reducers', () => store.replaceReducer(require('./reducers/index.js').default));
+}
