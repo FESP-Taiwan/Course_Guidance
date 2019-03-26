5cbebfee1a1602f138dc51467866e61dad2305b3
yan <ian24257587@gmail.com>
2019-03-26 16:10:32 +0800

    MainPage extract

---
+++ client/components/Main/MainPage.jsx
@@ -0,0 +1,116 @@
+// @flow
+
+import React, { PureComponent } from 'react';
+import radium from 'radium';
+
+const borderAnimation = radium.keyframes({
+  '0%': {
+    width: 105,
+    height: 105,
+    right: 20,
+    top: 20,
+    opacity: 1,
+  },
+  '100%': {
+    width: 145,
+    height: 145,
+    right: 0,
+    top: 0,
+    opacity: 0,
+  },
+}, 'blend');
+
+const styles = {
+  mainWrapper: {
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
+  start: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  line: {
+    width: 100,
+    height: 3,
+    backgroundColor: '#ffffff',
+  },
+  btnWrapper: {
+    border: 'none',
+    padding: 0,
+    width: 145,
+    height: 145,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'relative',
+    borderRadius: '50%',
+  },
+  circle: {
+    width: 105,
+    height: 105,
+    backgroundColor: '#ffffff',
+    borderRadius: '50%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    zIndex: 300,
+    transitionDuration: '0.5s',
+    ':hover': {
+      width: 145,
+      height: 145,
+    },
+  },
+  text: {
+    fontSize: 20,
+    position: 'relative',
+    lineHeight: 3,
+    backgroundColor: 'transparent',
+    textAlign: 'center',
+    transitionDuration: '0.5s',
+    ':hover': {
+      fontSize: 25,
+    },
+  },
+  circleBorder: {
+    border: 'solid 3px #ffffff',
+    backgroundColor: 'transparent',
+    width: 105,
+    height: 105,
+    position: 'absolute',
+    opacity: 0.5,
+    right: 10,
+    top: 10,
+    borderRadius: '50%',
+    animationName: borderAnimation,
+    animationDuration: '1s',
+    animationIterationCount: 'infinite',
+    animationTimingFunction: 'ease',
+  },
+};
+
+type Props = {
+
+};
+
+class MainPage extends PureComponent<Props> {
+  render() {
+    return (
+      <main style={styles.mainWrapper}>
+        <div style={styles.start}>
+          <button style={styles.btnWrapper} type="button">
+            <div style={styles.circleBorder} />
+            <div style={styles.circle} key="circle">
+              <span style={styles.text} key="text">START</span>
+            </div>
+          </button>
+          <div style={styles.line} />
+        </div>
+      </main>
+    );
+  }
+}
+
+export default radium(MainPage);
--- client/containers/MainBoard.jsx
+++ client/containers/MainBoard.jsx
@@ -1,6 +1,9 @@
 // @flow
 
-import React, { PureComponent } from 'react';
+import React, {
+  PureComponent,
+  Fragment,
+} from 'react';
 import {
   Switch,
   Route,
@@ -12,23 +15,8 @@ import radium from 'radium';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
 import background from '../static/images/project-p1-bgi.png';
-
-const borderAnimation = radium.keyframes({
-  '0%': {
-    width: 105,
-    height: 105,
-    right: 20,
-    top: 20,
-    opacity: 1,
-  },
-  '100%': {
-    width: 145,
-    height: 145,
-    right: 0,
-    top: 0,
-    opacity: 0,
-  },
-}, 'blend');
+import SelectionPage from './CourseGuidingPage/SelectionPage';
+import MainPage from '../components/Main/MainPage';
 
 const styles = {
   placement: {
@@ -44,73 +32,15 @@ const styles = {
     backgroundRepeat: 'no-repeat',
     backgroundSize: 'cover',
   },
-  mainWrapper: {
+  screen: {
     width: '100%',
     height: '100%',
     display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-  },
-  start: {
-    display: 'flex',
-    alignItems: 'center',
-  },
-  line: {
-    width: 100,
-    height: 3,
-    backgroundColor: '#ffffff',
-  },
-  btnWrapper: {
-    border: 'none',
-    padding: 0,
-    width: 145,
-    height: 145,
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    position: 'relative',
-    borderRadius: '50%',
-  },
-  circle: {
-    width: 105,
-    height: 105,
-    backgroundColor: '#ffffff',
-    borderRadius: '50%',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    zIndex: 300,
-    transitionDuration: '0.5s',
-    ':hover': {
-      width: 145,
-      height: 145,
-    },
-  },
-  text: {
-    fontSize: 20,
-    position: 'relative',
-    lineHeight: 1,
-    backgroundColor: 'transparent',
-    textAlign: 'center',
-    transitionDuration: '0.5s',
-    ':hover': {
-      fontSize: 25,
-    },
-  },
-  circleBorder: {
-    border: 'solid 3px #ffffff',
-    backgroundColor: 'transparent',
-    width: 105,
-    height: 105,
-    position: 'absolute',
-    opacity: 0.5,
-    right: 10,
-    top: 10,
-    borderRadius: '50%',
-    animationName: borderAnimation,
-    animationDuration: '1s',
-    animationIterationCount: 'infinite',
-    animationTimingFunction: 'ease',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+    whiteSpace: 'no-wrap',
+    overflow: 'hidden',
+    transitionDuration: '1s',
   },
 };
 
@@ -122,20 +52,16 @@ class MainBoard extends PureComponent<Props> {
   render() {
     return (
       <div style={styles.placement}>
-        <main style={styles.mainWrapper}>
-          <div style={styles.start}>
-            <button style={styles.btnWrapper} type="button">
-              <div style={styles.circleBorder} />
-              <div style={styles.circle} key="circle">
-                <span style={styles.text} key="text">START</span>
-              </div>
-            </button>
-            <div style={styles.line} />
-          </div>
-        </main>
+        <div style={styles.screen}>
+          <MainPage />
+          <Route path="/courseGuidance" component={SelectionPage} />
+        </div>
       </div>
     );
   }
 }
 
-export default radium(MainBoard);
+export default compose(
+  withRouter,
+  radium,
+)(MainBoard);
